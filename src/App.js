29afcb65225cd9b951d4c6fbeb5c6abdf4d72165
otavio <otavio.nunes021@gmail.com>
2021-08-27 10:51:46 -0300

    start donate code

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import NavBar  from './component/NavBar';
 
 import Routes from './Routes'
 import { BrowserRouter } from 'react-router-dom'
+import Donete from './component/Donate';
 
 
 function App() {
   return (
       <div className="App">
             <NavBar />
+            
             <Routes />
+            <Donete />
       </div>
   );
 }
